b68b90fce700e9adeb28461acd5009731e1dbf1f
Chris Sullivan <csullivan@KSMConsulting.com>
2017-05-04 15:37:23 -0400

    added the context and initializer here

---
+++ spotter/Models/Spot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace spotter.Models
+{
+    public class Spot
+    {
+        public int ID { get; set; }
+        public Double lat { get; set; }
+        public Double lng { get; set; }
+    }
+}
